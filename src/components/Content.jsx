dab995a4aeb8e21726d9d765318840a528aa0b73
Jose vinicius francener sarmento <70924237+Jose-Vinicius@users.noreply.github.com>
2022-06-23 18:31:33 -0300

    Location complet

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -72,6 +72,11 @@ export default function Content(){
             <h2 className='subtitle'>Localização</h2>
 
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.5689009494413!2d-46.65031128549182!3d-23.54800291698316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce584962c42d73%3A0x4c4711f991f2f401!2zUi4gZGEgQ29uc29sYcOnw6NvLCA3MTcgLSBDb25zb2xhw6fDo28sIFPDo28gUGF1bG8gLSBTUCwgMDEzMDEtMDAw!5e0!3m2!1spt-BR!2sbr!4v1655943685970!5m2!1spt-BR!2sbr" ></iframe>
+
+            <div className='location'>
+                <input type="text" name="addres" id="addres" placeholder='Localização para entrega'/>
+                <Button class="button--location" text="Buscar"/>
+            </div>
         </main>
     )
 }
